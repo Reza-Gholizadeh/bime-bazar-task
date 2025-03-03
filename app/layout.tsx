8fb2c1f2f21e44a3cd0fb1161a41ac199e878bfd
reza gholizade <reza.gholizade@SF-09121.local>
2025-03-03 18:38:39 +0330

    chore: initial project setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const vazirMatn = Vazirmatn({
+  weight: ["400", "500", "700"],
+  variable: "--font-vazir-matn",
+  subsets: ["arabic"],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${vazirMatn.variable} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/orderCompletion/page.tsx
@@ -0,0 +1,3 @@
+export const OrderCompletion = () => {
+    return<>successfully</>
+}
